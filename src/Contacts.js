a365e025dbd23f15f760214085fba6103e9ccb30
Klimentina Ivanova <klimentina1988@gmail.com>
2023-03-25 21:58:15 +0200

    Adding Contacts file

---
+++ src/Contacts.js
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+import '../src/styles/Contacts.css'
+import {Link, useHistory} from 'react-router-dom'
+import StorefrontTwoTone from '@material-ui/icons/StorefrontTwoTone'
+import { auth } from "./firebase";
+
+function Contacts(){
+
+}
+export default Contacts
