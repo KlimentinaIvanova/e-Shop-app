19cc127041ec733349482339b947c27b5c48d686
Klimentina Ivanova <klimentina1988@gmail.com>
2023-03-25 21:57:32 +0200

    Adding index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reducer,{initialState} from './reducer'
+import reducer, { initialState } from './reducer';
 import { StateProvider } from './StateProvider';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-  <StateProvider initialState={initialState} reducer={reducer}>
-  <App />
-  </StateProvider>
-  
-  
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <StateProvider initialState={initialState} reducer={reducer}>
+          <App />
+        </StateProvider>
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
