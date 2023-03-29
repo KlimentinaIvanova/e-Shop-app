14f9cf226ced34ba546f40cefd708ae713777dce
Klimentina Ivanova <klimentina1988@gmail.com>
2023-03-29 21:57:55 +0300

    Updating Header to display logged in user

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,25 +13,23 @@ const Header = () => {
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
   />
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-      <div className="img_logo">
-        <a href="/">
-          <img src="images/logo.png" alt="logo" />
-        </a>
-      </div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+ <div  class="head_top">
+       <div class="header">
+          <div class="container">
+             <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                   <div class="full">
+                      <div class="center-desk">
+                         <div class="logo"></div>
+        <a href="/"><img src="images/logo.png" alt="logo" /></a>
+        </div>
+                      </div>
+                   </div>
+                </div>
+                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                   <nav class="navigation navbar navbar-expand-md navbar-dark ">
       <div className="collapse navbar-collapse" id="navbarNav">
+        {user && user.email && <span>{user.email}</span>}
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <Link className="nav-link" to="/">
@@ -43,8 +41,8 @@ const Header = () => {
               Catalog
             </Link>
           </li>
-          {user && user.email ? (
-            <div id="user">
+          {user && user.email && <span>{user.email}</span> 
+           ? <div id="user">
               <li className="nav-item">
                 <Link className="nav-link" to="/create">
                   Create Product
@@ -55,13 +53,8 @@ const Header = () => {
                   Logout
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  {user.email}
-                </Link>
-              </li>
-            </div>
-          ) : (
+              </div>
+           : 
             <div id="guest">
               <li className="nav-item">
                 <Link className="nav-link" to="/login">
@@ -74,7 +67,7 @@ const Header = () => {
                 </Link>
               </li>
             </div>
-          )}
+          }
           <li className="nav-item">
             <Link className="nav-link" to="/contacts">
               Contact Us
@@ -93,15 +86,30 @@ const Header = () => {
           </button>
         </form>
       </div>
+      </nav>
     </div>
-  </nav>
+    </div>
+  </div>
   <section className="banner_main">
+  <div class="container-fluid">
+             <div class="row d_flex">
+                <div class="col-md-5">
     <div className="text-bg">
       <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ea minima quam autem iste voluptatem fugit quod praesentium, neque vitae consequuntur harum aliquid facilis natus, voluptatibus explicabo assumenda ex soluta.
+    Welcome to eShop!
       </h1>
+    <span>We are offering electronic technique with a great quality and the best price!</span>
     </div>
+    </div>
+    <div class="col-md-7 padding_right1">
+                   <div class="text-img">
+                      <figure><img src="images/top_img.png" alt="#"/></figure>
+                      </div>
+                </div>
+             </div>
+          </div>
   </section>
+  </div>
 </header>
   
     );
