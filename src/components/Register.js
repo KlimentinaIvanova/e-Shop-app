605f4a5276c597ce4ea3fcbada6d750f90af7608
Klimentina Ivanova <klimentina1988@gmail.com>
2023-03-29 18:13:43 +0300

    Updaiting Register to poin to Home

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,44 @@
 import './css/Register.css'
+import { useNavigate } from 'react-router-dom';
+import { useContext,useState } from 'react';
+import { AuthContext } from '../Services/AuthContext';
+import { auth } from '../firebase';
+
 const Register=()=>{
+
+    
+    const navigate=useNavigate()
+    const [errorMessage, setErrorMessage] = useState(null);
+    const {register}=useContext(AuthContext)
+    const {login}=useContext(AuthContext)
+
+const onSubmit=(e)=>{
+    e.preventDefault()
+
+    const formData=new FormData(e.target)
+
+    const user=formData.get('user')
+    const email=formData.get('email')
+    const password=formData.get('password')
+    const repass=formData.get('repass')
+
+    if(password !==repass){
+        return
+    }
+    auth.createUserWithEmailAndPassword(email,password)
+    .then(useCreditential=>{
+        const {user}=useCreditential
+        register(user)
+        navigate('/')
+    })
+    .catch((error)=>{
+        setErrorMessage(error.message)
+    })
+}
+
     return(
 
-            <form id='register'>
+            <form id='register'onSubmit={onSubmit}>
         <div className="register"/>
             <h1>Register</h1>
             <label htmlFor='email'>Email:</label>
@@ -14,7 +50,7 @@ const Register=()=>{
             <label htmlFor="pass">Password:</label>
             <input type='password' id='register-password' name='password'/>
             <label htmlFor='con-pass'>Confirm Password</label>
-            <input type='password' name='confirm password' id='confirm-password'/>
+            <input type='password' name='repass' id='confirm-password'/>
             <div className='register_button'>
             
             <input className="btn submit" type='submit' defaultValue='Register'/>
