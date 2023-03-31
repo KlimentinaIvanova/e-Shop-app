c244829b1e54a57470daba5b3895fa937d7ed3d9
Klimentina Ivanova <klimentina1988@gmail.com>
2023-03-31 19:55:12 +0300

    Adding NotFound page

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound=()=>{
+    return (
+        <div>
+       <h1>404 Not Found</h1>
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqI3lHFjBbLelg5rGnkZVukHUI2cd9cnEGOQ&usqp=CAU" alt="not_found"/>
+       </div>
+    )
+}
+export default NotFound
