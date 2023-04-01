c489356ed67f53c6edfd0986bc953bfca98f0d67
Klimentina Ivanova <klimentina1988@gmail.com>
2023-04-01 20:17:22 +0300

    Updating Catalog navigation

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,11 +1,21 @@
 import './css/Catalog.css'
 import {CatalogItem} from './CatalogItem'
+//import {  Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
  export const Catalog=({products})=>{
     console.log(products)
     return(
+      <div className='additional_navigation'>
+        <Link className="nav-link" to="/create">
+                  Create Product
+                </Link>
+                <Link className="nav-link" to="/">
+              Home
+            </Link>
         <div className="container">
         <div className="all_products">
+        
           <h2 className="section-title">
             <span className="px-2">All Products</span>
           </h2>
@@ -15,8 +25,12 @@ import {CatalogItem} from './CatalogItem'
           {products.length === 0 && (
             <h3 className="no-articles">No products yet</h3>
           )}
-        </div>
+    
+        </div>        
       </div>
+      </div>
+     
+     
     )
 }
 export default Catalog
