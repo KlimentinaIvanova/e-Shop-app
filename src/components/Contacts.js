d66fb680c47bdc624bc44562383f3f2336a79ec3
Klimentina Ivanova <klimentina1988@gmail.com>
2023-04-02 19:39:03 +0300

    Updating Contacts.js and Contacts.css

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,18 +1,19 @@
 import React from "react"
 import './css/Contacts.css'
+import { Link } from "react-router-dom"
 const Contacts = () => {
     return (
+        
         <div className="cont_call">
-            
-                {" "}
-                <h1 className="multi">We are here for you! </h1>
-                <h1>Contact Us:</h1>
-                <h2>Phone: </h2>
-                <p> (+1) 12345667890</p>
-                <h2>Or send us email:</h2>
-                <p>eshop@gmail.com</p>
-            
-        </div>
+        <div className="nav_contacts"><Link className="nav-link" to="/">Home</Link></div>
+        <h1 className="multi">We are here for you! </h1>
+        <h1>Contact Us:</h1>
+        <h2>Phone: </h2>
+        <p> (+1) 12345667890</p>
+        <h2>Or send us email:</h2>
+        <p>eshop@gmail.com</p>
+      </div>
+      
     )
 }
 export default Contacts
