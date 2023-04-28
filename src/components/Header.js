1e1b2804c0c27478f847098412e6fba2fd7c01a6
Klimentina Ivanova <klimentina1988@gmail.com>
2023-04-28 19:25:56 +0300

    Removing Search

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import './css/Header.css'
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Services/AuthContext";
 import { useContext } from "react";
+//import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
 
 const Header = () => {
 
@@ -13,21 +14,21 @@ const Header = () => {
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
   />
- <div  class="head_top">
-       <div class="header">
-          <div class="container">
-             <div class="row">
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                   <div class="full">
-                      <div class="center-desk">
-                         <div class="logo"></div>
+ <div  className="head_top">
+       <div className="header">
+          <div className="container">
+             <div className="row">
+                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                   <div className="full">
+                      <div className="center-desk">
+                         <div className="logo"></div>
         <a href="/"><img src="images/logo.png" alt="logo" /></a>
         </div>
                       </div>
                    </div>
                 </div>
-                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                   <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                   <nav className="navigation navbar navbar-expand-md navbar-dark ">
       <div className="collapse navbar-collapse" id="navbarNav">
         {user && user.email && <span>{user.email}</span>}
         <ul className="navbar-nav ms-auto">
@@ -73,27 +74,20 @@ const Header = () => {
               Contact Us
             </Link>
           </li>
+
+        <li className='nav-item'>
+       
+          </li>
         </ul>
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            <i className="fa fa-search"></i>
-          </button>
-        </form>
       </div>
       </nav>
     </div>
     </div>
   </div>
   <section className="banner_main">
-  <div class="container-fluid">
-             <div class="row d_flex">
-                <div class="col-md-5">
+  <div className="container-fluid">
+             <div className="row d_flex">
+                <div className="col-md-5">
     <div className="text-bg">
       <h1>
     Welcome to eShop!
@@ -101,8 +95,8 @@ const Header = () => {
     <span>We are offering electronic technique with a great quality and the best price!</span>
     </div>
     </div>
-    <div class="col-md-7 padding_right1">
-                   <div class="text-img">
+    <div className="col-md-7 padding_right1">
+                   <div className="text-img">
                       <figure><img src="images/top_img.png" alt="#"/></figure>
                       </div>
                 </div>
